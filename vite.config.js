6bc9ffc72082e0adef5e9dc80ff6cc0554758517
Michael <mikek222@gmail.com>
2026-01-21 14:40:50 -0500

    starter folders

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Eslintrc from './.eslintrc.cjs'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Eslintrc()],
+  plugins: [react()],
 })
