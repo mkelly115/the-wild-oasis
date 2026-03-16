e2325921b055064b2b86dfb39d7e94e21874e2a6
Michael <mikek222@gmail.com>
2026-03-16 13:39:41 -0400

    refactored button code - moved to individual UI file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -6,43 +9,25 @@ const H1 = styled.h1`
   background-color: yellow;
 `;
 
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-  cursor: pointer;
-`
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`
 const StyledApp = styled.div`
   background-color: orangered;
   padding: 20px;
-`
+`;
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={()=> alert("Check In!")}>Check in!</Button>
-      <Button onClick={()=> alert("Check Out!")}>Check out!</Button>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check In!")}>Check in!</Button>
+        <Button onClick={() => alert("Check Out!")}>Check out!</Button>
 
-      <Input type="number" placeholder="Number of guests" />
-      <Input type="number" placeholder="Number of guests" />
-    </StyledApp>
+        <Input type='number' placeholder='Number of guests' />
+        <Input type='number' placeholder='Number of guests' />
+      </StyledApp>
+    </>
   );
 }
 
 export default App;
-
-
-
-
-
