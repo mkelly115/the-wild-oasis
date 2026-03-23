274225fed12ccde5c357078feb6c1bf99fc49455
Michael <mikek222@gmail.com>
2026-03-23 13:29:07 -0400

    updated CSS styling and converted type to as

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,7 @@ import styled from "styled-components";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import GlobalStyles from "./styles/GlobalStyles";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
-
+import Heading from "./ui/Heading";
 const StyledApp = styled.div`
   background-color: orangered;
   padding: 20px;
@@ -19,10 +13,12 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
+        <Heading as='h1'>The Wild Oasis</Heading>
+        <Heading as='h2'>Check in and out</Heading>
         <Button onClick={() => alert("Check In!")}>Check in!</Button>
         <Button onClick={() => alert("Check Out!")}>Check out!</Button>
 
+        <Heading as='h3'>Form</Heading>
         <Input type='number' placeholder='Number of guests' />
         <Input type='number' placeholder='Number of guests' />
       </StyledApp>
