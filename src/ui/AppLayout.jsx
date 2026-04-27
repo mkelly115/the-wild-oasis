c6ea6720ae08e63faaf5c506044ffa77e210efd5
michael <mikek222@gmail.com>
2026-04-27 13:12:53 -0400

    updated styling and added toast

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,15 +4,23 @@ import SideBar from "./Sidebar";
 import styled from "styled-components";
 
 const StyledAppLayout = styled.div`
-    display: grid;
-    height: 100vh;
-    grid-template-columns: 26rem 1fr;
-    grid-template-rows: auto 1fr;
-`
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+`;
 
 const Main = styled.main`
-    background-color: var(--color-grey-50);
-    padding: 4rem 4.8rem 6.4rem;
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function AppLayout() {
@@ -21,7 +29,9 @@ function AppLayout() {
       <Header />
       <SideBar />
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
