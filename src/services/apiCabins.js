14b37dadded0c94ea48c074df53bfa6807b3b36b
michael <mikek222@gmail.com>
2026-05-05 18:14:54 -0400

    issed commit

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,7 +11,7 @@ export default async function getCabins() {
   return data;
 }
 
-export async function createEditCabin(newCabin){
+export async function createEditCabin(newCabin, id){
 
   const imageName = `${Math.random()}-${newCabin.image.name}`.replace('/',"");
 
