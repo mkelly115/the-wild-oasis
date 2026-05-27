c6007677c13eca7f204318a5e3dae3617ae347de
Michael <mikek222@gmail.com>
2026-05-27 12:44:09 -0400

    sorting of bookings feature

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,15 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import getCabins from "../../services/apiCabins";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, error, bookings };
