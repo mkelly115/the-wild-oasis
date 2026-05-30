f06f0425862aff0a60c293b094bb1a00ec86bf17
Michael <mikek222@gmail.com>
2026-05-30 13:13:28 -0400

    data fetching for stays and booking complete

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,19 @@
 import DashboardFilter from "../features/dashboard/DashboardFilter";
 import DashboardLayout from "../features/dashboard/DashboardLayout";
+import { useRecentBookings } from "../features/dashboard/useRecentBooking";
+import useRecentStays from "../features/dashboard/useRecentStays";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Spinner from "../ui/Spinner";
 
 function Dashboard() {
+  const { bookings, isLoadingBooking } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoadingStay } = useRecentStays();
+
+  if (isLoadingBooking || isLoadingStay) return <Spinner />;
+
+  console.log(bookings);
+
   return (
     <>
       <Row type='horizontal'>
