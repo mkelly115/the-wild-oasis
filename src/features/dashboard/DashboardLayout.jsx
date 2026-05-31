333d4b76d811ef5588cec23559cc24940b824469
Michael <mikek222@gmail.com>
2026-05-31 11:18:07 -0400

    stats fix for deployment

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Stats from "./stats";
+import Stats from "./Stats";
 import { useRecentBookings } from "./useRecentBooking";
 import { useRecentStays } from "./useRecentStays";
 import { useCabins } from "../cabins/useCabins";
